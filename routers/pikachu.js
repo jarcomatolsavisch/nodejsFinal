32d3409bcd5391bc4618ce53c9125018d3a01c08
James Ma <ma6036tp@gmail.com>
2021-07-04 00:10:38 +0800

    add pikachu's website

--- routers/pikachu.js
+++ routers/pikachu.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let router = express.Router();
+const path = require("path");
+router.use(express.static('./public'));
 let name = "Pikachu";
 let greeting = "pika! pika";
 router.use(function timelog(req,res,next){
@@ -9,7 +11,16 @@ router.use(function timelog(req,res,next){
 router.get('/',(req,res)=>{
     res.send(`${name}'s Home`);
 });
+//當訪問到/about這個router，回傳public底下的pikachu.html頁面
+//由於pikachu.html裡面需要用到一些同樣放在public的圖片
+//所以我在最開始的時候將public資料夾設為static
+//讓瀏覽器拿到pikachu.html之後可以去public獲取圖片
 router.get('/about',(req,res)=>{
-    res.send(greeting);
+    let options = {
+        //__dirname存的是目前pikachu.js所存在的檔案夾
+        root: path.join(__dirname, "../public"),
+        dotfiles: 'deny'
+    };
+    res.sendFile("pikachu.html",options);
 });
 module.exports = router;
